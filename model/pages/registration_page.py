e8a3f24b6a2bb0881cee7582fe7f6ca7505df40e
Olesya B <OBarkar@it-one.ru>
2024-04-12 00:08:34 +0300

    high-level-step-objects

--- model/pages/registration_page.py
+++ model/pages/registration_page.py
@@ -1,114 +1,48 @@
 import os
+import os.path
 
 from selene import have, command
 from selene.support.shared import browser
 
 
 class RegistrationPage:
-    def __init__(self):
-        self.first_name = browser.element('#firstName')
-        self.last_name = browser.element('#lastName')
-        self.email = browser.element("#userEmail")
-        self.other_gender = browser.element("[for ='gender-radio-3']")
-        self.user_number = browser.element("#userNumber")
-        self.subject = browser.element('#subjectsInput')
-        self.reading_hobby = browser.element("[for ='hobbies-checkbox-2']")
-        self.picture = browser.element("#uploadPicture")
-        self.address = browser.element("#currentAddress")
-        self.state = browser.element('#state')
-        self.city = browser.element('#city')
 
-        self.submit_button = browser.element("#submit")
-
-    def open(self):
+    def open_page(self):
         browser.open('/automation-practice-form')
-        browser.all('[id^=google_ads][id$=container__]').with_(timeout=10).wait_until(
-            have.size_greater_than_or_equal(3)
-        )
-        browser.all('[id^=google_ads][id$=container__]').perform(command.js.remove)
         return self
 
-    def fill_first_name(self, value):
-        self.first_name.type(value)
-        return self
+    def picture(self, user_photo):
+        root_path = os.getcwd()
+        return os.path.join(root_path, f"resources\\{user_photo}")
 
-    def fill_last_name(self, value):
-        self.last_name.type(value)
-        return self
-
-    def fill_email(self, value):
-        self.email.type(value)
-
-    def fill_other_gender(self):
-        return self.other_gender.click()
-
-    def fill_user_number(self, value):
-        return self.user_number.type(value)
-
-    def fill_date_of_birth(self, year, month, day):
+    def register(self, test_user):
+        browser.element('#firstName').type(test_user.first_name)
+        browser.element('#lastName').type(test_user.last_name)
+        browser.element('#userEmail').type(test_user.email)
+        browser.element('[for="gender-radio-3"]').click()
+        browser.element('#userNumber').type(test_user.user_number)
         browser.element('#dateOfBirthInput').click()
-        browser.element('.react-datepicker__month-select').type(month)
-        browser.element('.react-datepicker__year-select').type(year)
-        browser.element(
-            f'.react-datepicker__day--0{day}:not(.react-datepicker__day--outside-month)'
-        ).click()
-        return self
+        browser.element('.react-datepicker__year-select').type(test_user.birthday.year)
+        browser.element('.react-datepicker__month-select').type(test_user.birthday.month)
+        browser.element(f'.react-datepicker__day--0{test_user.birthday.day}').click()
+        browser.element('#subjectsInput').type(test_user.subject).press_enter()
+        browser.element('[for="hobbies-checkbox-2"]').click()
+        browser.element('#uploadPicture').send_keys(self.picture("picture.jpg"))  # (resource.path(test_user.photo))
+        browser.element('#currentAddress').type(test_user.address)
+        browser.element('#react-select-3-input').type(test_user.state).press_enter()
+        browser.element('#react-select-4-input').type(test_user.city).press_enter()
+        browser.element('#submit').perform(command.js.scroll_into_view).click()
 
-    def fill_subject(self, value):
-        return self.subject.type(value).press_enter()
-
-    def fill_reading_hobby(self):
-        return self.reading_hobby.click()
-
-    def upload_picture(self):
-        return self.picture().send_keys(os.path.abspath("resources/picture.jpg"))
-
-    def fill_address(self, value):
-        return self.address.type(value)
-
-    def fill_state(self, name):
-        self.state.perform(command.js.scroll_into_view)
-        self.state.click()
-        browser.all('[id^=react-select][id*=option]').element_by(
-            have.exact_text(name)
-        ).click()
-        return self
-
-    def fill_city(self, name):
-        self.city.click()
-        browser.all('[id^=react-select][id*=option]').element_by(
-            have.exact_text(name)
-        ).click()
-        return self
-
-    def submit(self):
-        return self.submit_button().click()
-
-    def should_registered_user_with(
-            self,
-            full_name,
-            email,
-            gender,
-            phone,
-            birthday,
-            subject,
-            hobby,
-            picture,
-            address,
-            city
-    ):
-        browser.element('.table').all('td').even.should(
-            have.exact_texts(
-                full_name,
-                email,
-                gender,
-                phone,
-                birthday,
-                subject,
-                hobby,
-                picture,
-                address,
-                city
-            )
-        )
-        return self
+    def should_have_registered(self, test_user):
+        browser.element('.table').all('tr td:nth-child(2)').should(have.texts(
+            f'{test_user.first_name} {test_user.last_name}',
+            test_user.email,
+            test_user.gender,
+            test_user.user_number,
+            f'{test_user.birthday.strftime("%B")}',
+            test_user.subject,
+            test_user.hobby,
+            test_user.picture,
+            test_user.address,
+            f'{test_user.state} {test_user.city}'
+        ))
--- tests/test_demoqa.py
+++ tests/test_demoqa.py
@@ -1,37 +1,27 @@
+import datetime
+
+from data.users import User
 from model.pages.registration_page import RegistrationPage
 
 
 def test_demoqa():
-    registration_page = RegistrationPage()
-    registration_page.open()
-
-    registration_page.fill_first_name("Oleg")
-    registration_page.fill_last_name("Teplov")
-
-    registration_page.fill_email("oteplov@gmail.com")
-    registration_page.fill_other_gender()
-    registration_page.fill_user_number("7123456789")
-
-    registration_page.fill_date_of_birth(1981, 3, 14)
-    registration_page.fill_subject("Hindi")
-    registration_page.fill_reading_hobby()
-    registration_page.upload_picture()
-    registration_page.fill_address("Some address")
-    registration_page.fill_state("NCR")
-    registration_page.fill_city("Noida")
-
-    registration_page.submit()
-
-    registration_page.should_registered_user_with(
-        'Oleg Teplov',
-        'oteplov@gmail.com',
-        'Other',
-        '7123456789',
-        '14 April,1981',
-        'Hindi',
-        'Reading',
-        'picture.jpg',
-        'Some address',
-        'NCR Noida',
+    test_user = User(
+        first_name='Oleg',
+        last_name='Teplov',
+        email='oteplov@gmail.com',
+        gender='Other',
+        user_number='7123456789',
+        birthday=datetime.date(1981, 4, 14),
+        subject='Hindi',
+        hobby='Reading',
+        picture='picture.jpg',
+        address='Some address',
+        state='NCR',
+        city='Noida'
     )
 
+    registration_page = RegistrationPage()
+    registration_page.open_page()
+    registration_page.register(test_user)
+
+    registration_page.should_have_registered(test_user)
