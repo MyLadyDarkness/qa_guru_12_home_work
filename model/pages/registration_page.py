ccc04fa740facb62a86f7bbf0dc8e789b1c601f7
Olesya B <OBarkar@it-one.ru>
2024-04-12 12:32:14 +0300

    Edit project according to comments:  add helper to find file path  change import  change directories to packages

--- model/pages/registration_page.py
+++ model/pages/registration_page.py
@@ -1,8 +1,6 @@
-import os
-import os.path
+from selene import browser, have, command
 
-from selene import have, command
-from selene.support.shared import browser
+from helper import path
 
 
 class RegistrationPage:
@@ -10,10 +8,6 @@ class RegistrationPage:
     def open_page(self):
         browser.open('/automation-practice-form')
         return self
-
-    def picture(self, user_photo):
-        root_path = os.getcwd()
-        return os.path.join(root_path, f"resources\\{user_photo}")
 
     def register(self, test_user):
         browser.element('#firstName').type(test_user.first_name)
@@ -27,7 +21,7 @@ class RegistrationPage:
         browser.element(f'.react-datepicker__day--0{test_user.birthday.day}').click()
         browser.element('#subjectsInput').type(test_user.subject).press_enter()
         browser.element('[for="hobbies-checkbox-2"]').click()
-        browser.element('#uploadPicture').send_keys(self.picture("picture.jpg"))  # (resource.path(test_user.photo))
+        browser.element('#uploadPicture').send_keys(path("picture.jpg"))  # (resource.path(test_user.photo))
         browser.element('#currentAddress').type(test_user.address)
         browser.element('#react-select-3-input').type(test_user.state).press_enter()
         browser.element('#react-select-4-input').type(test_user.city).press_enter()
