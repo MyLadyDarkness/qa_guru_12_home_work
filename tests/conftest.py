5fce9a9a9ab1a30b859abe2fe533c642f333cea8
Olesya B <OBarkar@it-one.ru>
2024-04-15 16:26:06 +0300

    fix fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -37,6 +37,9 @@ def selenoid_browser():
         options=options)
 
     browser.config.driver = driver
+    browser.config.base_url = 'https://demoqa.com'
+    browser.config.window_height = 1920
+    browser.config.window_width = 1080
 
     yield browser
 
