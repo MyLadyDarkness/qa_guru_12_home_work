59f4a9ba463552f193ea64c18656a5bf8224f6be
Olesya B <OBarkar@it-one.ru>
2024-04-15 16:07:12 +0300

    change unresolved url

--- tests/conftest.py
+++ tests/conftest.py
@@ -31,7 +31,8 @@ def selenoid_browser():
 
     options.capabilities.update(selenoid_capabilities)
     driver = webdriver.Remote(
-        command_executor=f"https://{selenoid_login}:{selenoid_pass}@{selenoid_url}/wd/hub",
+        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        #command_executor=f"https://{selenoid_login}:{selenoid_pass}@{selenoid_url}/wd/hub",
         options=options)
 
     browser.config.driver = driver
